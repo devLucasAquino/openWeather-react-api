1ff45e4c813375bf3e9476248ec9733321dc469c
PauloBezz <paulo.bezerra2057@gmail.com>
2024-05-15 22:24:45 -0300

    added useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import Card from './Card';
-
+import { useState } from 'react';
 
 import './App.css';
 
 function App() {
   const dayWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+  const [isDay, setIsDay] = useState('Sunday');
+
+  function handleIsDay(day){
+    setIsDay(day);
+  }
+
   return (
 
     <div className='app'>
       {
-        dayWeek.map((day) => {
+        dayWeek.map((day) => { 
           return (
-            <Card dayWeek={day}></Card>
+            <Card dayWeek={(day)}
+            style={{
+              backgroundColor: isDay === 'Sunday' && 'black',
+            }}></Card>
           )
         })
       }
