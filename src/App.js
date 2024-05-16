fdb5d63214034d1d7b6be19cf13628845cc37c8c
PauloBezz <paulo.bezerra2057@gmail.com>
2024-05-15 22:15:42 -0300

    feat: created Card.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import Card from './Card';
+
+
 import './App.css';
 
 function App() {
+  const dayWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='app'>
+      {
+        dayWeek.map((day) => {
+          return (
+            <Card dayWeek={day}></Card>
+          )
+        })
+      }
+
     </div>
+    
   );
 }
 
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+export default function Card( props ){
+    return(
+        <div className="Card">
+            <h1> {props.dayWeek} </h1>
+            <p>São Paulo</p>
+            <p>19ºC</p>
+        </div>
+    )
+}
